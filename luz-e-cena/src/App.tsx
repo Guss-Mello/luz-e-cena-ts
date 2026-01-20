5c0645dab44296b989f8275c0cc9b41de011ac45
Gustavo Mello Soares dos Santos <gustavo@digicom.com.br>
2026-01-20 20:25:39 -0300

    Criando e ajustando as primeiras partes do projeto em Type Script, baixei todas as imagens e fontes solicitadas e criei os componentes de Link e Banner

---
+++ luz-e-cena/src/App.tsx
@@ -0,0 +1,13 @@
+import Banner from "./components/Banner"
+import Link from "./components/Link";
+
+function App() {
+  return (
+    <>
+    <Banner src="./Imagens/Banner_principal/Banner_principal_desktop.png" alt="Banner"/>
+    <Link href="/" target="_blank">Link</Link>
+    </>
+  );
+}
+
+export default App
