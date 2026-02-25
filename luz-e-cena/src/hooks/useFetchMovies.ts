6c2de67285422ec2f9772855a8c7d836dc152a23
Gustavo Mello Soares dos Santos <gustavo@digicom.com.br>
2026-02-25 20:24:40 -0300

    criei um hook para tirar a "responsabilidade" de pegar os filmes do MovieSection, deixei la apenas para interrface, a busca ficou separada nesse hook

---
+++ luz-e-cena/src/hooks/useFetchMovies.ts
@@ -0,0 +1,31 @@
+import { Movie } from '../types';
+import { useEffect, useState } from 'react';
+import { getMovies } from '../api';
+
+const useFetchMovies = () => {
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const fetchMovies = async () => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const movies = await getMovies();
+            setMovies(movies);
+        } catch (err) {
+            setError("Erro ao buscar filmes. Tente novamente mais tarde.");
+            console.error(err);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchMovies();
+    });
+
+    return { movies, isLoading, error };
+}
+
+export default useFetchMovies
